ff85c06bbff3b62517775638d1b49f86df306fc7
c.gomez <carlosgomzp21@gmail.com>
2024-10-10 19:01:13 +0200

    metodos completos en la capa de negocio

--- AD_ACT_01_Fichero/src/modelo/negocio/GestorVideojuego.java
+++ AD_ACT_01_Fichero/src/modelo/negocio/GestorVideojuego.java
@@ -14,10 +14,11 @@ public class GestorVideojuego {
 	 * 
 	 * @param v el videojuego a guardar
 	 * @return <b>0</b> el videojuego pasado por parametro es null, <b>1</b> el nombre
-	 *         esta vacio o solo tiene espacios en blanco, <b>2</b> el password esta
-	 *         vacio o solo tiene espacios en blanco, <b>3</b> el usuario se ha
-	 *         guardado <b>666</b> en caso de que haya algún problema en el de
-	 *         entrada salida
+	 *         esta vacio o solo tiene espacios en blanco, o tiene menos de 3 letras
+	 *         <b>2</b> la compañia esta vacia o solo tiene espacios en blanco, o tiene 
+	 *         menos de 5 letras<b>3</b> la nota es inferior a 0 o superior a 100 <b>4</b>
+	 *         el videojuego se ha registrado correctamente <b>666</b> en caso de que 
+	 *         haya algún problema en el de entrada salida
 	 */
 	public int guardar(Videojuego v) {
 		if (v == null) {
@@ -26,14 +27,15 @@ public class GestorVideojuego {
 
 		dvf = new DaoVideojuegoFichero();
 		try {
-			if (v.getNombre().isBlank()) {
+			if (v.getNombre().isBlank() || v.getNombre().length() < 3) {
 				return 1;
-			} else if (v.getCompania().isBlank()) {
+			} else if (v.getCompania().isBlank() || v.getCompania().length() < 5) {
 				return 2;	
-			} else if (v.getNota().isBlank()) {
+			} else if (v.getNota() < 0 || v.getNota() > 100) {
+				return 3;
 			}else {
 				dvf.registrar(v);
-				return 3;
+				return 4;
 			}
 		} catch (Exception e) {
 			return 666;
@@ -42,58 +44,61 @@ public class GestorVideojuego {
 	
 	/**
 	 * Método que valida un videojuego pasado por parametro contra un videojuego 
-	 * guardado en la persistencia. Un usuario esta validado cuando el nombre, 
+	 * guardado en la persistencia. Un videojuego esta validado cuando el nombre, 
 	 * la empresa y la nota guardada coincide con el videojuego pasado por parametro.
 	 * 
 	 * @param v videojuego a validar/comparar
 	 * @return <b>0</b> el videojuego no existe, <b>1</b> el videojuego existe y es
-	 *         valido, <b>2</b> el videojuego existe pero no es valido y <b>3</b> en
+	 *         valido, <b>2</b> el videojuego existe pero no es valido y <b>666</b> en
 	 *         caso de que haya algún problema en el de entrada salida
 	 */
-	public int validar(Usuario u) {
-		duf = new DaoUsuarioFichero();
+	public int validar(Videojuego v) {
+		dvf = new DaoVideojuegoFichero();
 		try {
-			Usuario uFichero = duf.getByName(u.getNombre());
-			if (uFichero == null) {
+			Videojuego vFichero = dvf.getByName(v.getNombre());
+			if (vFichero == null) {
 				return 0;
 			}
 
-			if (uFichero.equals(u)) {
+			if (vFichero.equals(v)) {
 				return 1;
 			} else {
 				return 2;
 			}
 		} catch (Exception e) {
-			return 3;
+			return 666;
 		}
 	}
-	
 	/**
-	 * Metedo que agrega un videojuego pasado por parametro
-	 * @param nombre nombre del videojuego que se quiere guardar
-	 * @param compania nombre de la compania que se quiere agregar
-	 * @param nota valor entero de la nota que tiene el videojuego
-	 * @throws Exception 
+	 * Metodo que borra un videojuego pasado por parámetro
+	 * @param v videojuego a borrar
+	 * @return <b>0</b> el videojuego no existe, <b>1</b> el videojuego se ha 
+	 * 			borrado correctamente <b>2</b> el videojuego existe pero no es
+	 * 		 	valido y <b>666</b> en caso de que haya algún problema en el de 
+	 * 			entrada salida
 	 */
-	public void agregarVideojuego(String nombre, String compania, String nota) throws Exception {
-        // Validaciones
-        if (nombre.length() < 3) {
-            throw new Exception("El nombre del videojuego debe tener al menos 3 letras.");
-        }
-
-        if (compania.length() < 5) {
-            throw new Exception("La compañía debe tener al menos 5 letras.");
-        }
-
-        try {
-            int notaNumerica = Integer.parseInt(nota);
-            if (notaNumerica < 0 || notaNumerica > 100) {
-                throw new Exception("La nota debe ser un número entero entre 0 y 100.");
-            }
-            DaoVideojuegoFichero.guardarVideojuego(nombre, compania, notaNumerica);
-        } catch (NumberFormatException e) {
-            throw new Exception("La nota debe ser un número entero.");
-        }
-    }
+	public int borrar(Videojuego v) {
+		
+		if (v == null) {
+			return 0;
+		}
+		
+		dvf = new DaoVideojuegoFichero();
+		try {
+			
+			if(validar(v) == 1) {
+				dvf.borrarVideojuego(v);
+				return 1;
+			}else if(validar(v) == 2) {
+				return 2;
+				
+			}else{
+				return 0; //Esperar a probar el programa, puede fallar!!!
+			}
+		} catch (Exception e) { 
+			return 666;
+		}
+		
+	}
 
 }
