8da210b7763e7b5aaa767d544be92b25348b4c28
Dani Costas <daniel.costas.rodriguez@bootcamp-upgrade.com>
2024-10-10 16:23:39 +0200

    Add Interface DaoVideojuego & Class DaoVideojuegoFichero. Unimplemented methods yet

---
+++ AD_ACT_01_Fichero/src/modelo/persistencia/DaoVideojuegoFichero.java
@@ -0,0 +1,25 @@
+package modelo.persistencia;
+
+import modelo.entidad.Videojuego;
+
+public class DaoVideojuegoFichero implements IDaoVideojuego {
+
+	@Override
+	public void registrar(Videojuego vj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Videojuego getListaVideojuegos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean borrarVideojuego(Videojuego vj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
